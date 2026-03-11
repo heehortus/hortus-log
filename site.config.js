e0c0301c616a51e566f8bee6c8645cc0edcd65e3
heehortus <hee.hortus@gmail.com>
2026-03-11 19:21:51 +0900

    style: 이름 변경

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "hortus",
+    name: "정희원",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "UX/UI | Product Designer",
     bio: "ongoing designer",
