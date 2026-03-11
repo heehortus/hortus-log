d5e9feed44cbd5aebe0898c0d71287f862130426
heehortus <hee.hortus@gmail.com>
2026-03-11 19:17:22 +0900

    style: 모바일 프로필 카드 UI 텍스트 한글 변경

--- src/routes/Feed/MobileProfileCard.tsx
+++ src/routes/Feed/MobileProfileCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 const MobileProfileCard: React.FC<Props> = () => {
   return (
     <StyledWrapper>
-      <div className="top">💻 Profile</div>
+      <div className="top">✋ 프로필</div>
       <div className="mid">
         <div className="wrapper">
           <Image
