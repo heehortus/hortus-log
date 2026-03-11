9d8dd576f1075fab4b7027e197794918281d4d71
heehortus <hee.hortus@gmail.com>
2026-03-11 19:11:59 +0900

    style: UI 텍스트 한국어 변경 및 ServiceCard 제거

--- src/routes/Feed/SearchInput.tsx
+++ src/routes/Feed/SearchInput.tsx
@@ -8,12 +8,12 @@ const SearchInput: React.FC<Props> = ({ ...props }) => {
   return (
     <StyledWrapper>
       <div className="top">
-        <Emoji>🔎</Emoji> Search
+        <Emoji>🔎</Emoji> 검색
       </div>
       <input
         className="mid"
         type="text"
-        placeholder="Search Keyword..."
+        placeholder="키워드를 입력하세요."
         {...props}
       />
     </StyledWrapper>
