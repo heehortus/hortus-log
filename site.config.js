2118d3056202271d76de1ae593dc2067a753e11f
heehortus <hee.hortus@gmail.com>
2026-03-11 20:39:56 +0900

    feat: Google Search Console 연동

--- site.config.js
+++ site.config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
-      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "SUDRXQMPPwKJaO_iRJM3fORc1_JyTikOUkLpWz1Fh4A",
     },
   },
   naverSearchAdvisor: {
