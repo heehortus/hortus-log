16f3d675e05dd817e4bb73708be375172a1261a4
heehortus <hee.hortus@gmail.com>
2026-03-11 19:15:33 +0900

    style: 프로필 UI 텍스트 한글 변경

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -10,7 +10,7 @@ const ProfileCard: React.FC<Props> = () => {
   return (
     <StyledWrapper>
       <div className="title">
-        <Emoji>💻</Emoji> 프로필
+        <Emoji>✋</Emoji> 프로필
       </div>
       <div className="content">
         <div className="top">
